da83929a07ccde7ac58445b7195424b93c78c900
Ganesha Upadhyaya <ganeshrvce@gmail.com>
2020-08-13 10:37:29 -0700

    add random selection of tokens

--- contracts/sale/BQSale.sol
+++ contracts/sale/BQSale.sol
@@ -4,6 +4,13 @@ import "./FixedSupplyLotSale.sol";
 import "../token/BeastQuest.sol";
 
 contract BQSale is FixedSupplyLotSale {
+    uint256[] public _nonFungibleSupply;
+    uint256 public _seed;
+    uint256 public _lastUsedSeed;
+    uint256 public constant _MIN_SUPPLY = 50;
+
+    event RunningOutOfSupply(uint256 currentSupply);
+
     constructor(
         address payable payoutWallet,
         IERC20 payoutTokenAddress,
@@ -18,6 +25,69 @@ contract BQSale is FixedSupplyLotSale {
             inventoryContract
         )
     {}
+
+    /**
+     * @dev Sets or updates the nonFungibleSupply for the sale.
+     * @param nonFungibleSupply supply to be updated.
+     */
+    function updateNonFungibleSupply(uint256[] memory nonFungibleSupply)
+        public
+        onlyOwner
+    // whenNotStarted
+    {
+        require(nonFungibleSupply.length > 0);
+        _nonFungibleSupply = nonFungibleSupply;
+    }
+
+    function setRandomnessSeed(uint256 seed) public onlyOwner // whenNotStarted
+    {
+        _seed = seed;
+    }
+
+    function _randomlySelectAndUpdate(uint256 numTokens)
+        internal
+        returns (uint256[] memory)
+    {
+        uint256 supplySize = _nonFungibleSupply.length;
+
+        require(supplySize >= numTokens, "not enough supply");
+        require(_lastUsedSeed != _seed, "seed not updated from last purchase");
+
+        _lastUsedSeed = _seed;
+
+        uint256[] memory randomizedSupply = new uint256[](supplySize);
+
+        for (uint256 index = 0; index < supplySize; index++) {
+            uint256 tokenId = _nonFungibleSupply[index];
+            uint256 newIndex = uint256(
+                keccak256(abi.encode(tokenId, now, block.number, _seed))
+            ) % supplySize;
+            uint256 newNewIndex = newIndex;
+            while (randomizedSupply[newNewIndex] != 0) {
+                newNewIndex = (newNewIndex + 1) % supplySize;
+            }
+            newIndex = newNewIndex;
+            randomizedSupply[newIndex] = tokenId;
+        }
+
+        uint256[] memory selected = new uint256[](numTokens);
+        for (uint256 index = 0; index < numTokens; index++) {
+            selected[index] = randomizedSupply[index];
+        }
+
+        uint256[] memory newSupply = new uint256[](supplySize - numTokens);
+        for (uint256 index = numTokens; index < supplySize; index++) {
+            newSupply[index - numTokens] = randomizedSupply[index];
+        }
+
+        _nonFungibleSupply = newSupply;
+
+        if (_nonFungibleSupply.length < _MIN_SUPPLY) {
+            emit RunningOutOfSupply(_nonFungibleSupply.length);
+        }
+
+        return selected;
+    }
 
     function _purchaseForDelivery(PurchaseForVars memory purchaseForVars)
         internal
