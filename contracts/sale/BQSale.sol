68a0cde09ed58cca9a182065f7e21bc110b61096
Ganesha Upadhyaya <ganeshrvce@gmail.com>
2020-09-22 11:56:23 -0700

    remove external seeder, use blockhash solidity api

--- contracts/sale/BQSale.sol
+++ contracts/sale/BQSale.sol
@@ -6,13 +6,14 @@ import "../token/BeastQuest.sol";
 contract BQSale is FixedSupplyLotSale {
     uint256[] public _nonFungibleSupply;
     uint256 public _supplyOffset;
-    uint256 public _seed;
-    uint256 public _lastUsedSeed;
+    bytes32 public _lastUsedSeed;
     uint256 public constant _MIN_SUPPLY = 50;
 
     event RunningOutOfSupply(uint256 currentSupply, uint256 supplyOffset);
 
-    IERC20 public constant payoutTokenAddress = IERC20(0x00eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee);
+    IERC20 public constant payoutTokenAddress = IERC20(
+        0x00eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+    );
 
     constructor(
         address payable payoutWallet,
@@ -33,14 +34,16 @@ contract BQSale is FixedSupplyLotSale {
      * @dev Sets or updates the nonFungibleSupply for the sale.
      * @param nonFungibleTokens supply to be updated.
      */
-    function updateNonFungibleSupply(uint256[] memory nonFungibleTokens, uint256 supplyOffset)
-        public
-        onlyOwner
-    // whenNotStarted
+    function updateNonFungibleSupply(
+        uint256[] memory nonFungibleTokens,
+        uint256 supplyOffset
+    ) public onlyOwner // whenNotStarted
     {
         require(nonFungibleTokens.length > 0);
 
-        uint256 newSupplySize = _nonFungibleSupply.length.add(nonFungibleTokens.length);
+        uint256 newSupplySize = _nonFungibleSupply.length.add(
+            nonFungibleTokens.length
+        );
         uint256[] memory newNonFungibleSupply = new uint256[](newSupplySize);
 
         for (uint256 index = 0; index < _nonFungibleSupply.length; index++) {
@@ -56,24 +59,16 @@ contract BQSale is FixedSupplyLotSale {
         _supplyOffset = supplyOffset;
     }
 
-    function setRandomnessSeed(uint256 seed) public onlyOwner // whenNotStarted
-    {
-        _seed = seed;
-    }
-
     function _randomlySelectAndUpdate(uint256 quantity)
         internal
         override
-        returns 
-        (
-            uint256[] memory
-        )
+        returns (uint256[] memory)
     {
         uint256 supplySize = _nonFungibleSupply.length;
-
         require(supplySize >= quantity, "not enough supply");
-        require(_lastUsedSeed != _seed, "seed not updated from last purchase");
 
+        bytes32 _seed = blockhash(block.number - 1);
+        require(_lastUsedSeed != _seed, "seed not updated from last purchase");
         _lastUsedSeed = _seed;
 
         uint256[] memory randomizedSupply = new uint256[](supplySize);
